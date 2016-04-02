ba132e08dff407f666f00c4c1b6d9dd5017f6184
Andrew Nesbitt <andrewnez@gmail.com>
2016-04-02 18:59:27 +0100

    Flush redis between tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@ require 'rubygems'
 require 'bundler/setup'
 require 'split'
 require 'split/export'
+
+RSpec.configure do |config|
+  config.order = 'random'
+  config.before(:each) do
+    Split.configuration = Split::Configuration.new
+    Split.redis.flushall
+  end
+end
