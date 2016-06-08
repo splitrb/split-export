95c2ec29a6ff663159ad1598789796055541e106
Andrew Nesbitt <andrewnez@gmail.com>
2016-06-08 20:58:07 +0100

    Use fakeredis for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,15 @@ require 'rubygems'
 require 'bundler/setup'
 require 'split'
 require 'split/export'
+require 'fakeredis'
+
+fakeredis = Redis.new
 
 RSpec.configure do |config|
   config.order = 'random'
   config.before(:each) do
     Split.configuration = Split::Configuration.new
+    Split.redis = fakeredis
     Split.redis.flushall
   end
 end
