ce74c78ed35d86d5f830dedb5e7fc2dad1458feb
Maksim Mikityanskiy <maksim@ohcintl.com>
2016-12-12 15:54:55 -0500

    fixing completion count when using goals

--- lib/split/export.rb
+++ lib/split/export.rb
@@ -14,11 +14,13 @@ module Split
       csv = CSV.generate do |csv|
         csv << ['Experiment', 'Alternative', 'Participants', 'Completed', 'Conversion Rate', 'Z score', 'Control', 'Winner']
         Split::ExperimentCatalog.all.each do |experiment|
+          goals = experiment.goals + [nil] # nil corresponds to conversions without any goals.
+
           experiment.alternatives.each do |alternative|
             csv << [experiment.name,
                     alternative.name,
                     alternative.participant_count,
-                    alternative.completed_count,
+                    goals.inject(0) { |sum, g| sum + alternative.completed_count(g) },
                     round(alternative.conversion_rate, 3),
                     round(alternative.z_score, 3),
                     alternative.control?,
