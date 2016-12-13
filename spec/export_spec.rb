6982a54af176a073101b08b7ef47f50fcc5fc474
Maksim Mikityanskiy <maksim@ohcintl.com>
2016-12-13 18:17:02 -0500

    increased test samples so z-scores are nonzero

--- spec/export_spec.rb
+++ spec/export_spec.rb
@@ -4,22 +4,22 @@ describe Split::Export do
   before :each do
     experiment = Split::ExperimentCatalog.find_or_create({'link_color': ["control", "goal"]}, 'blue', 'red', 'green')
     blue = Split::Alternative.new('blue', 'link_color')
-    blue.participant_count = 5
-    blue.set_completed_count(2, "control")
-    blue.set_completed_count(1, "goal")
+    blue.participant_count = 50
+    blue.set_completed_count(20, "control")
+    blue.set_completed_count(10, "goal")
     blue.save
     red = Split::Alternative.new('red', 'link_color')
-    red.participant_count = 6
-    red.set_completed_count(2, "control")
-    red.set_completed_count(2, "goal")
+    red.participant_count = 60
+    red.set_completed_count(10, "control")
+    red.set_completed_count(30, "goal")
     red.save
   end
 
   it "should generate a csv of split data" do
-    Split::Export.to_csv.should eql("Experiment,Alternative,Participants,Completed,Conversion Rate,Z score,Control,Winner\nlink_color,blue,5,3,0.6,0.0,true,false\nlink_color,red,6,4,0.667,0.0,false,false\nlink_color,green,0,0,0.0,0.0,false,false\n")
+    Split::Export.to_csv.should eql("Experiment,Alternative,Participants,Completed,Conversion Rate,Z score,Control,Winner\nlink_color,blue,50,30,0.6,0.0,true,false\nlink_color,red,60,40,0.667,0.724,false,false\nlink_color,green,0,0,0.0,0.0,false,false\n")
   end
 
   it "should generate a detailed csv of split data for a single table" do
-    Split::Export.experiment_to_csv("link_color").should eql("Alternative,Goal,Participants,Completed,Conversion Rate,Z Score,Control,Winner\nblue,,5,0,0.0,N/A,true,false\nblue,control,5,2,0.4,N/A,true,false\nblue,goal,5,1,0.2,N/A,true,false\nred,,6,0,0.0,Needs 30+ participants.,false,false\nred,control,6,2,0.3333333333333333,Needs 30+ participants.,false,false\nred,goal,6,2,0.3333333333333333,Needs 30+ participants.,false,false\ngreen,,0,0,0,Needs 30+ participants.,false,false\ngreen,control,0,0,0,Needs 30+ participants.,false,false\ngreen,goal,0,0,0,Needs 30+ participants.,false,false\n")
+    Split::Export.experiment_to_csv("link_color").should eql("Alternative,Goal,Participants,Completed,Conversion Rate,Z Score,Control,Winner\nblue,,50,0,0.0,0.0,true,false\nblue,control,50,20,0.4,0.0,true,false\nblue,goal,50,10,0.2,0.0,true,false\nred,,60,0,0.0,0.0,false,false\nred,control,60,10,0.167,-2.736,false,false\nred,goal,60,30,0.5,3.257,false,false\ngreen,,0,0,0.0,0.0,false,false\ngreen,control,0,0,0.0,0.0,false,false\ngreen,goal,0,0,0.0,0.0,false,false\n")
   end
 end
